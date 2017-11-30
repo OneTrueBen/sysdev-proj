21e07300729b83f7f0965ce2cb592f043d1a50dd
cstuser <cstuser>
2017-11-30 09:12:56 -0500

    Css donegit add .

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
   end
+  layout false; #This is so it differs from the other pages in which it will not contain red team! followed by links
 end
